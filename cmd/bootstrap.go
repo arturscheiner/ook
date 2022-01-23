155e72798e60ce14b67fa3d1fecc158caddfe606
Artur Scheiner <artur.scheiner@gmail.com>
2022-01-23 09:02:37 -0300

    initial commit

---
+++ cmd/bootstrap.go
@@ -0,0 +1,76 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"ook/koo"
+	"os"
+
+	git "github.com/go-git/go-git/v5"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+)
+
+// bootstrapCmd represents the bootstrap command
+var bootstrapCmd = &cobra.Command{
+	Use:   "bootstrap",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		get_ook()
+		check_dependencies()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(bootstrapCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// bootstrapCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// bootstrapCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func get_ook() {
+	// Clone the given repository to the given directory
+	var AppFs = afero.NewOsFs()
+
+	userdir, err := os.UserHomeDir()
+	koo.CheckErr(err)
+
+	target_dir := userdir + "/.ook"
+
+	tde, err := afero.DirExists(AppFs, target_dir)
+	koo.CheckErr(err)
+
+	if tde {
+		fmt.Println(target_dir + " directory already exists!")
+		return
+	}
+	_, err = git.PlainClone(target_dir, false, &git.CloneOptions{
+		URL:      "https://github.com/arturscheiner/.ook.git",
+		Progress: os.Stdout,
+	})
+
+	koo.CheckErr(err)
+}
+
+func check_dependencies() {
+	ve := koo.CommandExists("vagrant")
+	if ve {
+		fmt.Println("vagrant is installed, check plugins for this system")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,37 @@
+module ook
+
+go 1.17
+
+require (
+	github.com/go-git/go-git/v5 v5.4.2
+	github.com/janeczku/go-spinner v0.0.0-20150530144529-cf8ef1d64394
+	github.com/melbahja/goph v1.3.0
+	github.com/spf13/afero v1.6.0
+	github.com/spf13/cobra v1.3.0
+)
+
+require (
+	github.com/Microsoft/go-winio v0.4.16 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/sftp v1.13.4 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+)
