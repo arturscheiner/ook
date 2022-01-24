ee5d42bed5157c67c0a89d3e688c734eccfd49fc
Artur Scheiner <artur.scheiner@gmail.com>
2022-01-24 14:02:25 -0300

    some changes building version 20220124.170225

--- koo/koo.go
+++ koo/koo.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/janeczku/go-spinner"
-	"github.com/k0kubun/go-ansi"
 	"github.com/melbahja/goph"
 	"github.com/schollz/progressbar/v3"
 )
@@ -26,35 +25,11 @@ func CheckErr(e error) {
 }
 
 func Bar(len int64, desc string) {
-	doneCh := make(chan struct{})
-
-	bar := progressbar.NewOptions(100,
-		progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
-		progressbar.OptionEnableColorCodes(true),
-		progressbar.OptionSetWidth(50),
-		progressbar.OptionSetTheme(progressbar.Theme{
-			Saucer:        " ",
-			AltSaucerHead: "[yellow]<[reset]",
-			SaucerHead:    "[yellow]-[reset]",
-			SaucerPadding: "[white]•",
-			BarStart:      "[blue]|[reset]",
-			BarEnd:        "[blue]|[reset]",
-		}),
-		progressbar.OptionOnCompletion(func() {
-			doneCh <- struct{}{}
-		}),
-	)
-
-	go func() {
-		for i := 0; i < 100; i++ {
-			bar.Add(1)
-			time.Sleep(10 * time.Millisecond)
-		}
-	}()
-
-	// got notified that progress bar is complete.
-	<-doneCh
-	fmt.Println("\n ======= progress bar completed ==========\n")
+	bar := progressbar.DefaultBytes(len, desc)
+	for i := 0; i < 100; i++ {
+		bar.Add(1)
+		time.Sleep(40 * time.Millisecond)
+	}
 }
 
 func OokSsh(user string, password string, server string, command string) {
