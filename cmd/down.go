d5719d7fec554249180b72cd1899bdb3df582480
Artur Scheiner <artur.scheiner@gmail.com>
2022-01-24 16:03:34 -0300

    some changes building version 20220124.190334

--- cmd/down.go
+++ cmd/down.go
@@ -23,8 +23,8 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		c := make(chan string)
-		go koo.Bar(-1, "executing", c)
-		down(c)
+		go down(c)
+		koo.Bar(-1, "executing", c)
 	},
 }
 
