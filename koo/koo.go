17754b1b065f8f1d2b05b119b7aec837d82ca0bc
Artur Scheiner <artur.scheiner@gmail.com>
2022-01-24 16:07:34 -0300

    some changes building version 20220124.190734

--- koo/koo.go
+++ koo/koo.go
@@ -27,7 +27,7 @@ func CheckErr(e error) {
 
 func Bar(len int64, desc string, c chan string) {
 	bar := progressbar.DefaultBytes(len, desc)
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 1000; i++ {
 		bar.Add(1)
 		time.Sleep(40 * time.Millisecond)
 	}
