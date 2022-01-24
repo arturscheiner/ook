c971598e00b36464767736c2946d193f7420af1e
Artur Scheiner <artur.scheiner@gmail.com>
2022-01-24 16:08:16 -0300

    some changes building version 20220124.190816

--- koo/koo.go
+++ koo/koo.go
@@ -30,6 +30,9 @@ func Bar(len int64, desc string, c chan string) {
 	for i := 0; i < 1000; i++ {
 		bar.Add(1)
 		time.Sleep(40 * time.Millisecond)
+		if c != nil {
+			break
+		}
 	}
 	bar.Describe(<-c)
 	bar.Finish()
