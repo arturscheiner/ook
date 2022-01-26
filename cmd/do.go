ec019d2f3324ed74f8118b62198a0353b18efcd5
Artur Scheiner <artur.scheiner@gmail.com>
2022-01-26 15:29:34 -0300

    some changes building version 20220126.182934

--- cmd/do.go
+++ cmd/do.go
@@ -10,7 +10,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var do ook.Config
+var do ook.Run
 
 // doCmd represents the do command
 var doCmd = &cobra.Command{
@@ -23,7 +23,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		do.Run.Init()
+		do.Init()
 	},
 }
 
--- ook/config_structs.go
+++ ook/config_structs.go
@@ -7,7 +7,6 @@ import (
 type Config struct {
 	Fs  afero.Fs
 	Dir OokDir
-	Run
 }
 
 type OokDir struct {
